865192077c16bc42fd7ffc229709446da05471f0
loic <loic.senecat@bsd.fr>
2023-02-17 17:28:23 +0100

    add inputField

---
+++ src/components/InputField.tsx
@@ -0,0 +1,16 @@
+﻿import { TextField } from "@fluentui/react";
+
+export function InputField(message: any, fieldName: string) {
+  const handleOnChange = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue?: string | undefined
+  ) => {
+    message[fieldName] = newValue;
+  };
+
+  return (
+    <div>
+      <TextField onChange={handleOnChange} label={fieldName} />
+    </div>
+  );
+}
